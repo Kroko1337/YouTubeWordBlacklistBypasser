515f7502799bc3998f0cf225fb7e5d9fd291876e
Kroko1337 <z&tLDL9B6TXX9Dk^yLhVHyTeztb>
2021-10-03 20:44:11 +0200

    added reverse

--- main.py
+++ main.py
@@ -1,31 +1,15 @@
-import copy
-
 import clipboard
-import pip
-import pkg_resources
-
-from pkg_resources import DistributionNotFound, VersionConflict
-
-dependencies = [
-    "Clipboard>=0.0.4"
-]
 
 
 def reverse(arg: str):
-    reversed
+    output = ""
     for x in range(1, len(arg) + 1):
-        reversed += arg[len(arg) - x]
-    return reversed
+        output += arg[len(arg) - x]
+    return output
 
 
-try:
-    pkg_resources.require(dependencies)
-except (VersionConflict, DistributionNotFound):
-    for dependency in dependencies:
-        split = dependency.split(">=")
-        pip.main(["install", split[0] + "==" + split[1]])
-        print("Install", split[0], "(" + split[1] + ")")
 inputText = input("What do you want to write?\n")
-bypassText = '\u202e' + inputText
+bypassText = '\u202e' + reverse(inputText)
+print(bypassText)
 clipboard.copy(bypassText)
 print("Copied the bypass text in clipboard!")
